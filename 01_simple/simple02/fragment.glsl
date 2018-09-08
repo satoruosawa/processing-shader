81ab134c68856da72aea3d2352da67f1f74002c1
SatoruOsawa <osawa@takram.com>
2018-09-08 11:25:51 +0900

    adjust shader

--- 01_simple/simple02/fragment.glsl
+++ 01_simple/simple02/fragment.glsl
@@ -1,11 +1,24 @@
 uniform sampler2D sampleTexture;
-uniform int textureSize;
-uniform int windowSize;
+uniform vec3 textureSize;
+uniform vec3 windowSize;
+
+// gl_FragCoord.xy
+// (0.5, 0.5) ... (texture.x - 0.5, 0.5)
+// ...
+// (0.5, texture.y - 0.5) ... (texture.x - 0.5, texture.y - 0.5)
+
+// texture2D samples below
+// (0.5 / textureSize.x, 0.5 / textureSize.y) ... ((texture.x - 0.5) / textureSize.x, 0.5 / textureSize.y)
+// ...
+// (0.5 / textureSize.x, (texture.y - 0.5) / textureSize.y) ... ((texture.x - 0.5) / textureSize.x, (texture.y - 0.5) / textureSize.y)
 
 void main() {
   vec4 tColor = texture2D(
     sampleTexture,
-    (floor(gl_FragCoord.xy * textureSize / windowSize) + 0.5) / textureSize
+    vec2(
+      (floor(gl_FragCoord.x * textureSize.x / windowSize.x) + 0.5) / textureSize.x,
+      (floor(gl_FragCoord.y * textureSize.y / windowSize.y) + 0.5) / textureSize.y
+    )
   );
   gl_FragColor = tColor;
 }
