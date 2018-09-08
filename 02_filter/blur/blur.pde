f35e26a1cb7c4b8a342878cee30e6fb9c65023a4
SatoruOsawa <osawa@takram.com>
2018-09-08 12:47:14 +0900

    bugfix blur

--- 02_filter/blur/blur.pde
+++ 02_filter/blur/blur.pde
@@ -5,21 +5,28 @@ void setup() {
   size(1000, 1000, P2D);
   background(255);
   V_BLUR = loadShader("fragment.glsl");
-  V_BLUR.set("textureSize", new PVector(width, height, 0));
-  V_BLUR.set("amount", 10.0);
+  V_BLUR.set("textureSize", new PVector(width, height));
+  V_BLUR.set("amount", 20);
   V_BLUR.set("isVertical", true);
   H_BLUR = loadShader("fragment.glsl");
-  H_BLUR.set("textureSize", new PVector(width, height, 0));
-  H_BLUR.set("amount", 10.0);
+  H_BLUR.set("textureSize", new PVector(width, height));
+  H_BLUR.set("amount", 20);
   H_BLUR.set("isVertical", false);
 }
 
 void draw() {
   background(255);
-  fill(0);
   noStroke();
+  fill(255, 0, 0);
+  rect(1, 1, 100, 100);
+  fill(0, 255, 0);
+  rect(width - 101, 1, 100, 100);
+  fill(255, 255, 0);
+  rect(1, height - 101, 100, 100);
+  fill(0, 0, 255);
+  rect(width - 101, height - 101, 100, 100);
+  fill(0);
   rect(mouseX, mouseY, 100, 100);
-  rect(100, 100, 100, 100);
   V_BLUR.set("sampleTexture", get());
   shader(V_BLUR); {
     rect(0, 0, width, height);
--- 02_filter/blur/fragment.glsl
+++ 02_filter/blur/fragment.glsl
@@ -1,26 +1,24 @@
 uniform sampler2D sampleTexture;
 uniform vec3 textureSize;
-uniform float amount; // 1 -
+uniform int amount; // more than 0
 uniform bool isVertical;
 
 void main(void)	{
   vec4 colorSum = vec4(0.0);
   float volumeSum = 0.0;
-  int resolution = int(amount); // any number
-  for (int i = -resolution; i <= resolution; i++) {
-    float shiftAmount = amount * float(i) / float(resolution);
-    vec2 shiftCoord = vec2(shiftAmount, 0.0);
+  for (int i = -amount; i <= amount; i++) {
+    vec2 shiftCoord = vec2(float(i), 0.0);
     if (isVertical) {
-      shiftCoord = vec2(0.0, shiftAmount);
+      shiftCoord = vec2(0.0, float(i));
     }
     vec4 color = texture2D(
       sampleTexture,
       vec2(
         (gl_FragCoord.x + shiftCoord.x) / textureSize.x,
-        ((textureSize.y - 1) - gl_FragCoord.y + shiftCoord.y) / textureSize.y
+        1.0 - (gl_FragCoord.y + shiftCoord.y) / textureSize.y
       )
     );
-    float volume = (float(resolution) + 1.0) - abs(float(i));
+    float volume = (float(amount) + 1.0) - abs(float(i));
     colorSum += volume * color;
     volumeSum += volume;
   }
